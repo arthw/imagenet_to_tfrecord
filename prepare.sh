67ccb4a80fe068ada56304bb50664de4d145e3a3
Zhang, Jianyu <jianyu.zhang@intel.com>
2021-01-20 14:54:40 +0800

    add init

---
+++ prepare.sh
@@ -0,0 +1,6 @@
+python3 -m venv tf_env
+source tf_env/bin/activate
+pip install --upgrade pip==19.3.1
+pip install intel-tensorflow
+pip install google-cloud-storage
+pip uninstall urllib3 && pip install urllib3
---
+++ run.sh
@@ -0,0 +1,10 @@
+IMAGENET_HOME=raw
+rm -rf $IMAGENET_HOME/tf_records/*
+rm -rf $IMAGENET_HOME/validation/*
+cd $IMAGENET_HOME/validation
+wget -O cat.JPEG https://raw.githubusercontent.com/intel/caffe/master/examples/images/cat.jpg
+cd -
+python3 imagenet_to_gcs.py \
+  --raw_data_dir=$IMAGENET_HOME \
+  --local_scratch_dir=$IMAGENET_HOME/tf_records \
+  --nogcs_upload
